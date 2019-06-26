bc2dd6f0164454150aeb8f79737dd2f314ee9730
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-06-26 15:30:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, Filters,Emoji
-app = Client("mxx",870831,"115641a0211dbd60dfdce6f367010e5f")
-@app.on_message(Filters.chat(-1001100924541) & Filters.text & ~Filters.edited)
+app = Client ("mnnn",768402,"f6420bf67303614279049d48d3e670f6")
+@app.on_message(Filters.chat(-1001262096355) & Filters.text & ~Filters.edited)
 def forward(client, message):
  f = False
  words = ['dekho','fix','😱','😢','😳','fixer','👆','👇','match','pass','sab','chase','defend','hai','karvana','link','loss','audio','varna','pura','puri','open','paid','contact','baazigar','market','load','whatsapp','timepass','kamma','book','teenpatti','diya','bhai','😀','😑','😐','😊','😜','😇','😎','😂','😘','😋','😝','🥺','members','🖕','member','only','chut','lund','gand','ma','maa','bhosdi','bahan','loude','lode','lavde','chutiya','🤞','🤟','☝️','mkc','bkc','mc','bc','madarchod','bahanchod','bahnchod','gandu','❓','kya','wbt','line','who',"https://",'joinchat','bullet','fuck','🤔','LUND',"LU","?","loda","lodu","telegram"]
@@ -8,12 +8,12 @@ def forward(client, message):
   if word.casefold() in message.text.casefold():
    f = True
  if not f:
-  mes = client.send_message(-1001274887387,"**" + message.text + "**")
+  mes = client.send_message(-1001378725482,"**" + message.text + "**")
   files = open("sure.txt" , "a")
   files.write(" " + str(message.message_id) +  " " + str(mes.message_id))
   files.close()
         
-@app.on_deleted_messages(Filters.chat(-1001100924541))
+@app.on_deleted_messages(Filters.chat(-1001262096355))
 def main(client, message):
  for v in message.messages:
   file = open("sure.txt" , "r")
@@ -23,10 +23,10 @@ def main(client, message):
    x = line.split()
    id = str(v.message_id )
    if id in x:
-    client.edit_message_text(-1001274887387,int(x[x.index(id)+1]), "." )
-    client.delete_messages(-1001274887387,int(x[x.index(id)+1]))
+    client.edit_message_text(-1001378725482,int(x[x.index(id)+1]), "." )
+    client.delete_messages(-1001378725482,int(x[x.index(id)+1]))
         
-@app.on_message(Filters.chat(-1001100924541)& Filters.text & Filters.edited)
+@app.on_message(Filters.chat(-1001262096355)& Filters.text & Filters.edited)
 def forward(client, message):
  file = open("sure.txt" , "r")
  lines = file.readlines()
@@ -35,7 +35,7 @@ def forward(client, message):
   x = line.split()
   id = str(message.message_id)
   if id in x:
-    client.edit_message_text(-1001274887387,int(x[x.index(id)+1]),"**" + message.text + "**")
+    client.edit_message_text(-1001378725482,int(x[x.index(id)+1]),"**" + message.text + "**")
      
 @app.on_message(Filters.command("clear"))
 def main(client, message):
