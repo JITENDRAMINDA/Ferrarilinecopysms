53d48b9e6fee7c3f7cecbd871c79afebb7a761f5
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-07-18 14:07:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from pyrogram import Client, Filters,Emoji
 
 app = Client("session",771202,"28eed966b0cd4238a4f4f8f0ab4c9c72")
 
-@app.on_message(Filters.text & ~Filters.edited)
+@app.on_message(Filters.channel,Filters.text & ~Filters.edited)
 def forward(client, message):
  fie = open("source.txt" , "r")
  lies = file.readlines()
@@ -33,7 +33,7 @@ def main(client, message):
       client.edit_message_text(-1001378725482,int(x[x.index(id)+1]), "." )
       client.delete_messages(-1001378725482,int(x[x.index(id)+1]))
         
-@app.on_message(Filters.chat(Filters.text & Filters.edited)
+@app.on_message(Filters.channel,Filters.text & Filters.edited)
 def forward(client, message):
    file = open("sure.txt" , "r")
    lines = file.readlines()
