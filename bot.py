a224beaf74a827040922e837afcac6ed5b76cc35
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-07-20 20:23:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, Filters,Emoji
 from pyrogram.errors import FloodWait
-app =  Client ("mnnn",768402,"f6420bf67303614279049d48d3e670f6")
+app =  Client ("mxx",870831,"115641a0211dbd60dfdce6f367010e5f")
 s = -1001100924541
 d = -1001274887387
 @app.on_message(Filters.chat(s) & Filters.text & ~Filters.edited)
