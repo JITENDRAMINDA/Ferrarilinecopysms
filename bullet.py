39bde7eb6f72cb59ef13e8307db94a1a22503938
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-07-25 10:16:09 +0530

    Update bullet.py

--- bullet.py
+++ bullet.py
@@ -4,9 +4,6 @@ import time
 
 app = Client("mnnn",768402,"f6420bf67303614279049d48d3e670f6")
 
-#("mnnn",bot_token="918985036:AAGuOy3Z00pcdp761gDouf5iEmnGz7rivR0",api_id=768402,api_hash="f6420bf67303614279049d48d3e670f6")
-
-
 s = -1001262096355
 d = -1001378725482
 @app.on_message(Filters.chat(s) & Filters.text & ~Filters.edited)
