46412c73e6380c81dce83727fcddce444f10093e
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-07-25 08:38:29 +0530

    Update linet.py

--- linet.py
+++ linet.py
@@ -2,8 +2,11 @@ from pyrogram import Client, Filters,Emoji
 from pyrogram.errors import FloodWait
 import time
 
-app = Client("mnnn",bot_token="918985036:AAGuOy3Z00pcdp761gDouf5iEmnGz7rivR0",api_id=768402,api_hash="f6420bf67303614279049d48d3e670f6")
-#("mnnn",768402,"f6420bf67303614279049d48d3e670f6")
+app = Client("mnnn",768402,"f6420bf67303614279049d48d3e670f6")
+
+#("mnnn",bot_token="918985036:AAGuOy3Z00pcdp761gDouf5iEmnGz7rivR0",api_id=768402,api_hash="f6420bf67303614279049d48d3e670f6")
+
+
 
 s = -1001316526418
 d = -1001464551111
