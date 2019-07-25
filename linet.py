9b30b17cfc93c6d63fa20e8201a01741a178f21d
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-07-25 08:26:39 +0530

    Update linet.py

--- linet.py
+++ linet.py
@@ -5,8 +5,8 @@ import time
 app = Client("mnnn",bot_token="918985036:AAGuOy3Z00pcdp761gDouf5iEmnGz7rivR0",api_id=768402,api_hash="f6420bf67303614279049d48d3e670f6")
 #("mnnn",768402,"f6420bf67303614279049d48d3e670f6")
 
-s = -1001262096355
-d = -1001378725482
+s = -1001316526418
+d = -1001464551111
 @app.on_message(Filters.chat(s) & Filters.text & ~Filters.edited)
 def forward(client, message):
  f = False
