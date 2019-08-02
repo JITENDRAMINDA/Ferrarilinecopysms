ba7bd284705f5ceef6600efad1a2765a50c9606d
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-08-02 16:32:32 +0530

    Update bulletreal.py

--- bulletreal.py
+++ bulletreal.py
@@ -12,7 +12,7 @@ def forward(client, message):
   if word.casefold() in message.text.casefold():
    f = True
  if not f:
-  mes = client.send_message(d,"**" + message.text.replace('🖲' , '💘').replace('📟' , '🏝️').replace('🎾' , '⚾') + "**")
+  mes = client.send_message(d,"**" + message.text + "**")
   files = open("sure.txt" , "a")
   files.write(" " + str(message.message_id) +  " " + str(mes.message_id))
   files.close()  
