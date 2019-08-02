4c815c1dc7c59e9b842cd343bd2d1d5eb0f2682c
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-08-02 12:31:22 +0530

    Update ferrarireal.py

--- ferrarireal.py
+++ ferrarireal.py
@@ -12,9 +12,9 @@ def forward(client, message):
   if word.casefold() in message.text.casefold():
    f = True
  if not f:
-   mes = client.send_message(d,message.text,parse_mode = "markdown" )
+   mes = client.send_message(d,message.text,parse_mode ="markdown")
    files = open("sure.txt" , "a")
-   files.write(" " + str(message.message_id) +  " " + str(mes.message_id))
+   files.write(" " + str(message.message_id) + " " + str(mes.message_id))
    files.close()
 @app.on_message(Filters.chat(s) & Filters.text & Filters.edited)
 def forward(client, message):
