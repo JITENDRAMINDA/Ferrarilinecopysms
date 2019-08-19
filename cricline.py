fded44f3e2405c33cbf59d33501471b0a366ecea
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-08-19 19:50:33 +0530

    Update cricline.py

--- cricline.py
+++ cricline.py
@@ -12,7 +12,7 @@ def forward(client, message):
   if word.casefold() in message.text.casefold():
    f = True
  if not f:
-  mes = client.send_message(d, "**" + message.text + "**")
+  mes = client.send_message(d, "**" + message.text.replace("🇩🇪","🇳🇮").replace("🎾","🥎").replace("🖲","🧤").replace("📟","🛑").replace("WD","🔷 WIDE BALL 🔷").replace("/","~").replace("CHALU RAKHO CHALU RAKHO ✔️✔️","GAME STARTED, PLAYERS ON THE STEDIUM ✔️✔️") + "**")
   files = open("sure.txt" , "a")
   files.write(" " + str(message.message_id) +  " " + str(mes.message_id))
   files.close()  
@@ -26,7 +26,7 @@ def forward(client, message):
   id = str(message.message_id)
   if id in x:
    try:
-    client.edit_message_text(d,int(x[x.index(id)+1]),"**" + message.text + "**")
+    client.edit_message_text(d,int(x[x.index(id)+1]),"**" + message.text.replace("🇩🇪","🇳🇮").replace("🎾","🥎").replace("🖲","🧤").replace("📟","🛑").replace("WD","🔷 WIDE BALL 🔷").replace("/","~").replace("CHALU RAKHO CHALU RAKHO ✔️✔️","GAME STARTED, PLAYERS ON THE STEDIUM ✔️✔️") + "**")
    except FloodWait as e:
     time.sleep(e.x)
 @app.on_deleted_messages(Filters.chat(s))
